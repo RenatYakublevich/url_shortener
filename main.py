89b6a666a52329f0481a036d40fea7b108e47f40
Renat Yakublevich <49581209+RenatYakublevich@users.noreply.github.com>
2021-01-20 20:14:13 +0300

    1.3
    add logging

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import sqlite3
 from flask import Flask, redirect, request, render_template, flash, g, url_for
 from db import Database
 import requests
+import logging
 
+
+logging.basicConfig(level=logging.DEBUG)
 
 DATABASE_FILE = 'db.db'
 DEBUG = True
@@ -11,10 +14,18 @@ SECRET_KEY = 'xzf39d7348yfui'
 HOST = 'http://127.0.0.1'
 PORT = '4997'
 
-
 app = Flask(__name__)
 app.config.from_object(__name__	)
 
+def error_handler(func):
+	def wrapper(*args,**kwargs):
+		try:
+			return func(*args,**kwargs)
+		except Exception as e:
+			logging.error(f'Some error!\n{e}')
+
+	wrapper.__name__ = func.__name__
+	return wrapper
 
 def connect_db():
 	conn = sqlite3.connect(app.config['DATABASE_FILE'])
@@ -26,27 +37,25 @@ def get_db():
 		g.link_db = connect_db()
 	return g.link_db
 
+@error_handler
 def validation_link(link: str) -> bool:
 	if link.startswith('https://'):
 		return True
 
-@app.teardown_appcontext
-def close_db(error):
-	if hasattr(g, 'link_db'):
-		g.link_db.close()
-
 @app.route('/',methods=['POST','GET'])
+@error_handler
 def index():
 	if request.method == 'POST':
 		if validation_link(request.form['link']):
-			requests.post(f'{HOST}:{PORT}{url_for("create_link")}',params={'link': request.form['link'], 'redirect_link': request.form['rlink']})
-			flash('Ссылка успешно добавлена!')
+			requests.post(f'{HOST}:{PORT}{url_for("add_link")}',params={'link': request.form['link'], 'redirect_link': request.form['rlink']})
+			flash('Ссылка успешно добавлена!', category='succes')
 		else:
-			flash('Ошибка добавления')
+			flash('Ошибка добавления', category='error')
 
 	return render_template('index.html',title='Главная')
 
 @app.route('/<path:link>')
+@error_handler
 def get_link(link):
 	try:
 		db = get_db()
@@ -56,10 +65,9 @@ def get_link(link):
 		return 'Такой ссылки нет!'
 
 @app.route('/create_link',methods=['POST','GET'])
-def create_link():
+def add_link():
 	db = get_db()
 	dbase = Database(db)
-	print(request.args.get('link'),request.args.get('redirect_link'))
 	dbase.add_link(request.args.get('link'),request.args.get('redirect_link'))
 	return redirect(url_for('index'))
 
